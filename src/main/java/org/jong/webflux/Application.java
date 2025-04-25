2d707bbc58b2510ebbd12f01806fa666d7e6f597
JongHae.Park <jong.park@linecorp.com>
2025-04-25 21:18:51 +0900

    MonoDefer Mono w/ value vs Mono w/ supplier

--- src/main/java/org/jong/webflux/Application.java
+++ src/main/java/org/jong/webflux/Application.java
@@ -1,7 +1,5 @@
 package org.jong.webflux;
 
-import org.jong.webflux.reactor.usage.MonoFluxErrorHandling;
-import org.jong.webflux.reactor.usage.MonoFluxTransformationAndMerge;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -12,8 +10,8 @@ public class Application {
     public static void main(String[] arg){
         ApplicationContext applicationContext = SpringApplication.run(Application.class, arg);
 
-        //SandBox reactorSandBox = applicationContext.getBean(MonoFluxErrorHandling.class);
-        //reactorSandBox.start();
+        SandBox reactorSandBox = applicationContext.getBean(SandBox.class);
+        reactorSandBox.start();
 
     }
 }
